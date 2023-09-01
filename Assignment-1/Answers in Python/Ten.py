2b8d115495c862e225f43e96634115ab2c5351a8
Vishal Kumar Maurya <118094180+vmaurya6622@users.noreply.github.com>
2023-09-01 20:22:46 +0530

    Update Ten.py

--- Assignment-1/Answers in Python/Ten.py
+++ Assignment-1/Answers in Python/Ten.py
@@ -9,7 +9,7 @@ expansion=0.2
 while abs(value(x0))>expansion:                 #iterating using the while near the X0 by getting its  values @ value(X0) and differentiation(x0)
     x0=x0-(value(x0))/differentiation(x0)
 print(round(x0,3))
-for i in range(1500):                           #maing the function more precise about finfing the roots
+for i in range(1500):                           #making the function more precise about finding the roots by increasing the loop counts.
     x1=x0-(value(x0))/differentiation(x0)       #increasing the values of X0 and usnig it as a next guess.
     if abs(x1-x0)<expansion:
         print(round(x1,3))
